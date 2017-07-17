20a90c32797abd975b745c71af9a079775b3dda9
Randy Champagne <r-champagne@neiu.edu>
2017-07-17 14:04:08 -0400

    installed middlewares n extended server

--- server.js
+++ server.js
@@ -1,48 +1,62 @@
-// Get dependencies
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
+/*
+*
+*   Express: ReST-API and Static-File server (with external route modules)
+*
+*/
 
-// Get our API routes
-const api = require('./server/routes/api');
 
+/**
+ * 	Get dependencies
+ **/
+const express 	 = require('express'),
+	  port 		 = process.env.PORT || '3000',
+	  api 		 = require('./server/routes/api');
+	  path 		 = require('path'),
+	  http 		 = require('http'),
+	  bodyParser = require('body-parser'),
+	  morgan     = require('morgan'),
+	  path 		 = require('path'),
+	  fs 		 = require('fs'),
+	  multer  	 = require('multer'),
+	  upload 	 = multer({ dest: 'public/uploads/' })
+;
+
+
+
+/**
+ *	init app. Attach & config middleware
+ **/
 const app = express();
-
-// Parsers for POST data
-app.use(bodyParser.json());
+app.set('port', port);										// Store port from environment in Express.
+app.use(morgan('dev'));										// use morgan for logging
+app.set('view engine', 'pug');								// use pug to render views
+app.use(bodyParser.json());									// Parsers for POST data
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'dist'))); 		// Point static path to dist
 
-// Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
 
-// Set our api routes
-app.use('/api', api);
 
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'dist/index.html'));
+/**
+ *	config routes
+ **/
+app.use('/api', api);										// Set our api routes
+app.get('*', (req, res) => {								// Other routes defer to Angular's routing module
+	res.sendFile(path.join(__dirname, 'dist/index.html'));	
 });
 
-/**
- * Get port from environment and store in Express.
- */
-const port = process.env.PORT || '3000';
-app.set('port', port);
+
 
 /**
- * Create HTTP server.
- */
+ *	Create HTTP server. Listen on provided port, on all network interfaces.
+ **/
 const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
 server
 	.listen(
 		port, 
 		() => console.log(`API running on localhost:${port}`)
 	)
 ;
+
+
 
 
