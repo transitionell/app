93adab33d78e2861129ef06c52e42a70ee0c84cf
Randy Champagne <r-champagne@neiu.edu>
2017-08-25 11:58:01 -0400

    need to fetch API creds from env vars & authenticate to Vision API

--- server/routes/api.js
+++ server/routes/api.js
@@ -68,10 +68,26 @@ router
 				req.file.path+'.jpg'
 			)
 
+
+			// Debug
+			console.log('projectId: ',process.env.GCLOUD_PROJECT);
+
 			/* get text from image */
 			let config = {
-				projectId: 'transition-ell',
-				keyFilename: 'server/_secrets/TransitionELL-f43d0dfba03a.json'
+				projectId: process.env.GCLOUD_PROJECT,
+				project_id: process.env.GCLOUD_PROJECT,
+				credentials: {
+					type: process.env.GCLOUD_TYPE,
+					project_id: process.env.GCLOUD_PROJECT,
+					private_key_id: process.env.GCLOUD_PRIVATE_KEY_ID,
+					private_key: process.env.GCLOUD_PRIVATE_KEY,
+					client_email: process.env.GCLOUD_CLIENT_EMAIL,
+					client_id: process.env.GCLOUD_CLIENT_ID,
+					auth_uri: process.env.GCLOUD_AUTH_URI,
+					token_uri: process.env.GCLOUD_TOKEN_URI,
+					auth_provider_x509_cert_url: process.env.GCLOUD_AUTH_PROVIDER_X509_CERT_URL,
+					client_x509_cert_url: process.env.GCLOUD_CLIENT_X509_CERT_URL
+				}
 			};
 			let client = require('@google-cloud/vision')(config);
 			client
