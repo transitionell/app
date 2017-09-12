5f031f59d5673d78d1c2addaaf90921cdb45c417
Randy Champagne <r-champagne@neiu.edu>
2017-09-12 01:57:37 -0400

    rm logic programatically authing to GCloud

--- server/routes/api.js
+++ server/routes/api.js
@@ -70,23 +70,7 @@ router
 
 
 			/* get text from image */
-			let config = {
-				projectId: process.env.GCLOUD_PROJECT,
-				project_id: process.env.GCLOUD_PROJECT,
-				credentials: {
-					type: process.env.GCLOUD_TYPE,
-					project_id: process.env.GCLOUD_PROJECT,
-					private_key_id: process.env.GCLOUD_PRIVATE_KEY_ID,
-					private_key: process.env.GCLOUD_PRIVATE_KEY,
-					client_email: process.env.GCLOUD_CLIENT_EMAIL,
-					client_id: process.env.GCLOUD_CLIENT_ID,
-					auth_uri: process.env.GCLOUD_AUTH_URI,
-					token_uri: process.env.GCLOUD_TOKEN_URI,
-					auth_provider_x509_cert_url: process.env.GCLOUD_AUTH_PROVIDER_X509_CERT_URL,
-					client_x509_cert_url: process.env.GCLOUD_CLIENT_X509_CERT_URL
-				}
-			};
-			let client = require('@google-cloud/vision')(config);
+			let client = require('@google-cloud/vision')();
 			client
 				.readDocument(req.file.path+'.jpg')
 				.then(
