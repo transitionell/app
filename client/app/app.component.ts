1734aa6397e30bc97472b4887fbe36fdda1d97e3
Randy Champagne <champagne.randy@gmail.com>
2017-09-30 00:37:12 -0400

    added logic to print envName

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,6 +1,9 @@
 import { 
 	Component 
 } 						from '@angular/core';
+import { 
+	environment 
+} 						from '../environments/environment';
 
 @Component({
 	selector: 'app-root',
@@ -9,4 +12,6 @@ import {
 })
 export class AppComponent {
 	
+	env: string 	 = environment.envName;
+	environment: any = environment;
 }
